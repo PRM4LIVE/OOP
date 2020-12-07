762bc94317b72a2a703329610e457b2d4e1c3701
user <wasdwas@mail.ru>
2020-12-07 16:18:29 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        AbstractPhone firstPhone = new ThomasEdisonPhone(1879);
+        AbstractPhone phone = new Phone(1984);
+        AbstractPhone videoPhone=new VideoPhone(2018);
+        User user = new User("Андрей");
+        user.callAnotherUser(224466,firstPhone);
+// Вращайте ручку
+
+//Сообщите номер абонента, сэр
+
+        user.callAnotherUser(224466,phone);
+//Вызываю номер 224466
+
+        user.callAnotherUser(224466,videoPhone);
+//Подключаю видеоканал для абонента 224466
+    }
+}
---
+++ src/main/java/WirelessPhone.java
@@ -0,0 +1,9 @@
+public abstract class WirelessPhone extends AbstractPhone {
+
+    private int hour;
+
+    public WirelessPhone(int year, int hour) {
+        super(year);
+        this.hour = hour;
+    }
+}
